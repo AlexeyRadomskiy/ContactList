2816628387950ff5e706dcd342e1c1e9bd73354c
Alexey <alexeyradomskiy98@gmail.com>
2022-03-14 20:27:27 +0300

    Setup Models

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,45 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Alexey on 13.03.2022.
+//
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        
+        let iterationCount = min(names.count, surnames.count, phones.count, emails.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                phone: phones[index],
+                email: emails[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
